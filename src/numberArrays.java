5f3f9794df89a08d85f572016c68b305596325d1
SamuelPalmhager <palmhagersamuel@gmail.com>
2023-04-26 13:16:38 +0200

    Lite arrayer

---
+++ src/numberArrays.java
@@ -0,0 +1,68 @@
+public class numberArrays {
+
+    private String[] player1number1 = new String[8];
+    private String[] player2number1 = new String[8];
+    private String[] player1number2 = new String[12];
+    private String[] player2number2 = new String[12];
+
+
+    public numberArrays(){
+        init();
+    }
+
+    public void init(){
+        //Number 1
+            player1number1[0] = "0,1";
+            player1number1[1] = "0,2";
+            player1number1[2] = "0,3";
+            player1number1[3] = "1,2";
+            player1number1[4] = "2,2";
+            player1number1[5] = "3,2";
+            player1number1[6] = "4,2";
+            player1number1[7] = "3,1";
+
+            player2number1[0] = "9,1";
+            player2number1[1] = "9,2";
+            player2number1[2] = "9,3";
+            player2number1[3] = "8,2";
+            player2number1[4] = "7,2";
+            player2number1[5] = "6,2";
+            player2number1[6] = "5,2";
+            player2number1[7] = "4,3";
+
+            //Number 2
+            player1number2[0] ="0,0";
+            player1number2[1] ="0,1";
+            player1number2[2] ="0,2";
+            player1number2[3] ="0,3";
+            player1number2[4] ="0,4";
+            player1number2[5] ="1,2";
+            player1number2[6] ="2,3";
+            player1number2[7] ="3,4";
+            player1number2[8] ="4,3";
+            player1number2[9] ="4,2";
+            player1number2[10] ="4,1";
+            player1number2[11] ="3,0";
+
+        //Number 2
+        player2number2[0] ="9,4";
+        player2number2[1] ="9,3";
+        player2number2[2] ="9,2";
+        player2number2[3] ="9,1";
+        player2number2[4] ="9,0";
+        player2number2[5] ="8,2";
+        player2number2[6] ="7,1";
+        player2number2[7] ="6,0";
+        player2number2[8] ="5,1";
+        player2number2[9] ="5,2";
+        player2number2[10] ="5,3";
+        player2number2[11] ="6,4";
+
+
+
+
+
+
+    }
+
+    }
