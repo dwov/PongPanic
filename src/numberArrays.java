437efc2537bb5fa0a922580e13c328e839e16699
SamuelPalmhager <palmhagersamuel@gmail.com>
2023-04-27 09:27:16 +0200

    Lite arrayer

--- src/numberArrays.java
+++ src/numberArrays.java
@@ -1,9 +1,27 @@
 public class numberArrays {
 
+    private String[] player1number0 = new String[12];
+    private String[] player2number0 = new String[12];
     private String[] player1number1 = new String[8];
     private String[] player2number1 = new String[8];
     private String[] player1number2 = new String[12];
     private String[] player2number2 = new String[12];
+    private String[] player1number3 = new String[9];
+    private String[] player2number3 = new String[9];
+    private String[] player1number4 = new String[9];
+    private String[] player2number4 = new String[9];
+    private String[] player1number5 = new String[12];
+    private String[] player2number5 = new String[12];
+    private String[] player1number6 = new String[10];
+    private String[] player2number6 = new String[10];
+    private String[] player1number7 = new String[13];
+    private String[] player2number7 = new String[13];
+    private String[] player1number8 = new String[9];
+    private String[] player2number8 = new String[9];
+    private String[] player1number9 = new String[9];
+    private String[] player2number9 = new String[9];
+
+
 
 
     public numberArrays(){
@@ -11,6 +29,35 @@ public class numberArrays {
     }
 
     public void init(){
+        //Number 0
+        player1number0[0] = "1,0";
+        player1number0[1] = "2,0";
+        player1number0[2] = "3,0";
+        player1number0[3] = "0,1";
+        player1number0[4] = "0,2";
+        player1number0[5] = "0,3";
+        player1number0[6] = "1,4";
+        player1number0[7] = "2,4";
+        player1number0[8] = "3,4";
+        player1number0[9] = "4,3";
+        player1number0[10] = "4,2";
+        player1number0[11] = "4,1";
+
+        player2number0[0] = "9,1";
+        player2number0[1] = "9,2";
+        player2number0[2] = "9,3";
+        player2number0[3] = "0,8";
+        player2number0[4] = "0,7";
+        player2number0[5] = "0,6";
+        player2number0[6] = "1,5";
+        player2number0[7] = "2,5";
+        player2number0[8] = "3,5";
+        player2number0[9] = "4,6";
+        player2number0[10] = "4,7";
+        player2number0[11] = "4,8";
+
+
+
         //Number 1
             player1number1[0] = "0,1";
             player1number1[1] = "0,2";
@@ -58,11 +105,261 @@ public class numberArrays {
         player2number2[10] ="5,3";
         player2number2[11] ="6,4";
 
+        //number 3
+
+        player1number3[0] = "1,1";
+        player1number3[1] = "2,0";
+        player1number3[2] = "1,4";
+        player1number3[3] = "3,0";
+        player1number3[4] = "3,2";
+        player1number3[5] = "4,3";
+        player1number3[6] = "3,4";
+        player1number3[7] = "2,4";
+        player1number3[8] = "1,3";
+
+        player2number3[0] = "2,9";
+        player2number3[1] = "1,9";
+        player2number3[2] = "0,8";
+        player2number3[3] = "1,7";
+        player2number3[4] = "0,6";
+        player2number3[5] = "1,5";
+        player2number3[6] = "2,5";
+        player2number3[7] = "3,6";
+        player2number3[8] = "?";
+
+        //Number 4
+
+        player1number4[0] = "4,0";
+        player1number4[1] = "4,1";
+        player1number4[2] = "2,4";
+        player1number4[3] = "4,2";
+        player1number4[4] = "4,3";
+        player1number4[5] = "4,4";
+        player1number4[6] = "3,2";
+        player1number4[7] = "2,2";
+        player1number4[8] = "2,3";
+
+        player2number4[0] = "0,9";
+        player2number4[1] = "0,8";
+        player2number4[2] = "0,7";
+        player2number4[3] = "0,6";
+        player2number4[4] = "0,5";
+        player2number4[5] = "1,7";
+        player2number4[6] = "2,7";
+        player2number4[7] = "2,6";
+        player2number4[8] = "2,5";
+
+        //Number 5
+
+        player1number5[0] = "1,0";
+        player1number5[1] = "2,0";
+        player1number5[2] = "3,0";
+        player1number5[3] = "4,1";
+        player1number5[4] = "4,2";
+        player1number5[5] = "3,2";
+        player1number5[6] = "2,2";
+        player1number5[7] = "1,2";
+        player1number5[8] = "1,3";
+        player1number5[9] = "2,4";
+        player1number5[10] = "3,4";
+        player1number5[11] = "4,4";
+
+        player2number5[0] = "3,9";
+        player2number5[1] = "2,9";
+        player2number5[2] = "1,9";
+        player2number5[3] = "0,9";
+        player2number5[4] = "0,8";
+        player2number5[5] = "0,7";
+        player2number5[6] = "1,7";
+        player2number5[7] = "2,7";
+        player2number5[8] = "3,7";
+        player2number5[9] = "3,6";
+        player2number5[10] = "2,5";
+        player2number5[11] = "1,5";
+        player2number5[12] = "0,5";
+
+        player1number6[0] = "2,0";
+        player1number6[1] = "3,0";
+        player1number6[2] = "4,1";
+        player1number6[3] = "3,2";
+        player1number6[4] = "2,2";
+        player1number6[5] = "1,1";
+        player1number6[6] = "0,2";
+        player1number6[7] = "1,3";
+        player1number6[8] = "2,4";
+        player1number6[9] = "3,4";
+
+        player2number6[0] = "2,9";
+        player2number6[1] = "1,9";
+        player2number6[2] = "0,8";
+        player2number6[3] = "1,7";
+        player2number6[4] = "2,7";
+        player2number6[5] = "3,8";
+        player2number6[6] = "4,7";
+        player2number6[7] = "3,6";
+        player2number6[8] = "2,5";
+        player2number6[9] = "1,5";
+
+        player1number7[0] = "0,4";
+        player1number7[1] = "1,4";
+        player1number7[2] = "2,4";
+        player1number7[3] = "3,4";
+        player1number7[4] = "4,4";
+        player1number7[5] = "0,2";
+        player1number7[6] = "1,2";
+        player1number7[7] = "2,2";
+        player1number7[8] = "3,2";
+        player1number7[9] = "4,2";
+        player1number7[10] = "0,0";
+        player1number7[11] = "1,1";
+        player1number7[12] = "3,3";
+
+        player2number7[0] = "5,4";
+        player2number7[1] = "5,3";
+        player2number7[2] = "5,2";
+        player2number7[3] = "5,1";
+        player2number7[4] = "5,0";
+        player2number7[5] = "7,4";
+        player2number7[6] = "7,3";
+        player2number7[7] = "7,2";
+        player2number7[8] = "7,1";
+        player2number7[9] = "7,0";
+        player2number7[10] = "9,4";
+        player2number7[11] = "8,3";
+        player2number7[12] = "6,1";
+
+        player1number8[0] = "1,0";
+        player1number8[1] = "2,0";
+        player1number8[2] = "3,0";
+        player1number8[3] = "0,1";
+        player1number8[4] = "4,1";
+        player1number8[5] = "1,2";
+        player1number8[6] = "2,2";
+        player1number8[7] = "2,3";
+        player1number8[8] = "0,3";
+        player1number8[9] = "4,3";
+        player1number8[10] = "1,4";
+        player1number8[11] = "2,4";
+        player1number8[12] = "3,4";
 
 
+        player2number8[0] = "3,9";
+        player2number8[1] = "2,9";
+        player2number8[2] = "1,9";
+        player2number8[3] = "4,8";
+        player2number8[4] = "0,8";
+        player2number8[5] = "3,7";
+        player2number8[6] = "2,7";
+        player2number8[7] = "1,7";
+        player2number8[8] = "6,4";
+        player2number8[9] = "3,5";
+        player2number8[10] = "2,5";
+        player2number8[11] = "1,5";
+        player2number8[12] = "0,6";
 
+        player1number9[0] = "4,0";
+        player1number9[1] = "4,1";
+        player1number9[2] = "4,2";
+        player1number9[3] = "4,3";
+        player1number9[4] = "4,4";
+        player1number9[5] = "3,1";
+        player1number9[6] = "2,1";
+        player1number9[7] = "1,2";
+        player1number9[8] = "1,3";
+        player1number9[9] = "3,4";
+        player1number9[10] = "2,4";
 
+        player2number9[0] = "4,9";
+        player2number9[1] = "4,8";
+        player2number9[2] = "4,7";
+        player2number9[3] = "4,6";
+        player2number9[4] = "4,5";
+        player2number9[5] = "3,8";
+        player2number9[6] = "2,8";
+        player2number9[7] = "1,7";
+        player2number9[8] = "1,6";
+        player2number9[9] = "3,5";
+        player2number9[10] = "2,5";
 
     }
 
+    public String[] getPlayer1number0() {
+        return player1number0;
     }
+
+    public String[] getPlayer2number0() {
+        return player2number0;
+    }
+
+    public String[] getPlayer1number1() {
+        return player1number1;
+    }
+
+    public String[] getPlayer2number1() {
+        return player2number1;
+    }
+
+    public String[] getPlayer1number2() {
+        return player1number2;
+    }
+
+    public String[] getPlayer2number2() {
+        return player2number2;
+    }
+
+    public String[] getPlayer1number3() {
+        return player1number3;
+    }
+
+    public String[] getPlayer2number3() {
+        return player2number3;
+    }
+
+    public String[] getPlayer1number4() {
+        return player1number4;
+    }
+
+    public String[] getPlayer2number4() {
+        return player2number4;
+    }
+
+    public String[] getPlayer1number5() {
+        return player1number5;
+    }
+
+    public String[] getPlayer2number5() {
+        return player2number5;
+    }
+
+    public String[] getPlayer1number6() {
+        return player1number6;
+    }
+
+    public String[] getPlayer2number6() {
+        return player2number6;
+    }
+
+    public String[] getPlayer1number7() {
+        return player1number7;
+    }
+
+    public String[] getPlayer2number7() {
+        return player2number7;
+    }
+
+    public String[] getPlayer1number8() {
+        return player1number8;
+    }
+
+    public String[] getPlayer2number8() {
+        return player2number8;
+    }
+
+    public String[] getPlayer1number9() {
+        return player1number9;
+    }
+
+    public String[] getPlayer2number9() {
+        return player2number9;
+    }
+}
