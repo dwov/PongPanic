1b63df19dfbcc78dfa57f50c9026cecbbe329f9b
dwov <35770053+dwov@users.noreply.github.com>
2023-05-30 09:53:30 +0200

    Arduino kod, fresh

--- ArduinoKOD/Pong_Panic_Finale_p2/Pong_Panic_Finale_p2.ino
+++ ArduinoKOD/Pong_Panic_Finale_p2/Pong_Panic_Finale_p2.ino
@@ -2,6 +2,25 @@
 #include <Ethernet.h> // Ethernet bibliotek för att hantera anslutningen, ethernet-controllern mm.
 #include <FastLED.h>  // Färdigt bibliotek för att kontrollera diverse led-controllers, bl.a. WS2112b
 
+/* Skriven 2023 av David Permlid
+ * 
+ * Det här är den slutgiltiga koden för det inbyggda systemet. I denna finns det funktioner som läser av 
+ * och tolkar meddelanden som i främsta form är koordinater mottagna från det servern. Den läser samtidigt 
+ * kontinuerligt av knapparna och berättar samt registrerar om en knapp detekteras som nedtryckt.
+ * 
+ * I filen har jag tagit användning av två färdiga bibliotek för att hjälpa mig kontrollera Ethernet_Shield
+ * samt ledslingan som används i kretsen.
+ * 
+ * Programmet sätter en hårdkodad IP- och MAC-adress som sedan skapar variabler och dylikt som behövs under
+ * körning. Sedan körs huvudprogrammet som kontinuerligt bekräftar att systemet är uppkopplat och vid brist
+ * på uppkoppling försöker systemet kontinuerligt återansluta. Om ansluten kollar den om knapparna är nedtryckta
+ * och om vi fått något nytt meddelande från servern.
+ * 
+ * Om vi mottager en koordinat så tänds lampan som motsvarar koordinaten på spelplanen. Vid mottaget "reset"
+ * så släcks lamporna. Knappar skickas till servern om den är i samma koloumn som "bollen" och startar en 
+ * timeout för alla knapparna på 500ms som hindrar spelaren från att klicka igen på den avsatta tiden.
+ */
+
 #define NUM_LEDS 25
 CRGB leds[NUM_LEDS];
 
@@ -18,13 +37,13 @@ const int NUM_BUTTONS = sizeof(BUTTON_PIN) / sizeof(BUTTON_PIN[0]);
 
 unsigned long lastHeartbeatTime = 0;  // Timer since last heartbeat (in milliseconds)
 unsigned long buttonTimer = 0;  // Timer for button presses (in milliseconds)
-unsigned long buttonTimer1 = 0;
+unsigned long buttonTimer1 = 0;  // Timer to send to server (in milliseconds)
 unsigned long buttonTimeout = 500; // Timeout duration for button presses (in milliseconds)
 
 int x = 0;
 int y = 0;
 int buttonState[NUM_BUTTONS];
-int lastButtonState[NUM_BUTTONS];
+int lastButtonState;
 
 void setup() {
   FastLED.addLeds<NEOPIXEL, 8>(leds, NUM_LEDS);
@@ -35,10 +54,10 @@ void setup() {
   for (int i = 0; i < NUM_BUTTONS; i++) {
     pinMode(BUTTON_PIN[i], INPUT_PULLUP);
     buttonState[i] = LOW;
-    lastButtonState[i] = LOW;
   }
+  lastButtonState = LOW;
 
-  delay(2500);
+  delay(2000);
   resetLights();
 }
 
@@ -61,6 +80,9 @@ void loop() {
   }
 }
 
+/* 
+ * Turns of all the lights (set all lights to black)
+ */
 void resetLights() {
   for(int i = 0; i < NUM_LEDS; i++) {
     leds[i] = CRGB::Black;
@@ -69,13 +91,9 @@ void resetLights() {
   Serial.println("reset_lights");
 }
 
-boolean checkButton(int index) {
-  if(digitalRead(BUTTON_PIN[index]) == HIGH && buttonState[index] == LOW) {
-    return true;
-  }
-  return false;
-}
-
+/*
+ * This function sends a "heartbeat" to the server if 3 seconds has passed.
+ */
 void handleHeartbeat() {
   if(millis() - lastHeartbeatTime >= 3000) {
     client.println("heartbeat");
@@ -83,11 +101,17 @@ void handleHeartbeat() {
   }
 }
 
+/*
+ * Reads the current state of all buttons. If a button has been pressed it will start a timeout,
+ * and block the user from pressing any buttons for a short period of time. If the button is in 
+ * the same coloum as the x coordinate, it will be sent to the server as a timer from when the 
+ * y coordinate last was 9.
+ */
 void readButtonState() {
   for(int i = 0; i < NUM_BUTTONS; i++) {
-    if(digitalRead(BUTTON_PIN[i]) == HIGH && buttonState[i] == LOW) {
+    if(digitalRead(BUTTON_PIN[i]) == HIGH && lastButtonState == LOW) {
       Serial.println("Button " + String(i) + " pressed");
-      buttonState[i] = HIGH;
+      lastButtonState = HIGH;
       buttonTimer = millis();
       if (x == i) {
         buttonTimer1 = millis() - buttonTimer1;
@@ -98,11 +122,15 @@ void readButtonState() {
 
   if (millis() - buttonTimer >= buttonTimeout) {
     for(int i = 0; i < NUM_BUTTONS; i++) {
-      buttonState[i] = LOW;
+      lastButtonState = LOW;
     }
   }
 }
 
+/*
+ * Checks if anything has been sent from the server and handles it appropriately, turning on lights,
+ * turn off lights.
+ */
 void processServerResponse() {
   String response = client.readStringUntil('\n');
   if (response.startsWith("reset")) {
@@ -145,11 +173,14 @@ void processServerResponse() {
   }
 }
 
+/*
+ * Tries to connect to the specified server ip, and port.
+ */
 boolean connectToServer() {
   Serial.println("Connecting to server...");
   resetLights();
   client.stop();
-  delay(1000);  // Wait for a second before attempting to reconnect
+  delay(100);  // Wait for a second before attempting to reconnect
   
   if (client.connect(serverIP, serverPort)) {
     lastHeartbeatTime = millis();  // Reset the heartbeat timer
